34a836eb56462bc2fb92cf54dad759ae43be4243
emilyrvillalba <56134620+emilyrvillalba@users.noreply.github.com>
2021-10-11 16:00:48 -0700

    A6: Emily's tests and setUp() & tearDown()

--- Backend/test.py
+++ Backend/test.py
@@ -3,14 +3,37 @@ import main
 
 class TestStringMethods(unittest.TestCase):
 
- 
-    #Flora
-    def test_check_username(self):
-        self.assertEqual(main.check_username("12csaver4e3w  "), False)
+ # Before
+  def setUp(self):
+    print("\n")
+    print("Before testing...")
+    
+    
+    
+  #Flora
+  def test_check_username(self)
+    self.assertEqual(main.check_username("12csaver4e3w  "), False)
 
-    def test_check_username2(self):
-        self.assertEqual(main.check_username("^*!(&#%($"), False)    
-        
+  def test_check_username2(self):
+    self.assertEqual(main.check_username("^*!(&#%($"), False)    
+      
+     
+  # Emily
+  def test_check_valid_password(self):
+    # ensures that password satisfies length requirement
+    password = main.check_valid_password("supercalifragilisticexpialidocious")
+    self.assertEqual(password, True)
+
+  def test_check_valid_password2(self):
+    # ensures that password DOES NOT satisfy length requirement
+    password = main.check_valid_password("hello")
+    self.assertEqual(password, False)
+
+    
+
+  # After
+  def tearDown(self):
+    print("Finishing test...")
 
 
 
