27324d174072fdc03c8b10567f9e626d41e5c200
emilyrvillalba <56134620+emilyrvillalba@users.noreply.github.com>
2021-10-11 15:48:29 -0700

    Emily's Tests and setup() & teardown()

--- Backend/test.py
+++ Backend/test.py
@@ -2,6 +2,12 @@ import unittest
 import main
 
 class TestStringMethods(unittest.TestCase):
+    # Before
+    def setUp(self):
+        print("\n")
+        print("Before testing...")
+
+ 
     #Flora
     def test_check_username(self):
         self.assertEqual(main.check_username("12csaver4e3w  "), False)
@@ -10,8 +16,19 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(main.check_username("^*!(&#%($"), False)    
         
     # Emily
-    def test_check_username_bool(self):
-        self.assertFalse(main.check_username("JohnnyJune"))
+     def test_check_username_bool(self):
+        # ensures that this username is not taken
+        username = main.check_username("JohnnyJune")
+        self.assertFalse(username)
+     def test_check_valid_password(self):
+        # ensures that password satisfies length requirement
+        password = main.check_valid_password("supercalifragilisticexpialidocious")
+        self.assertEqual(password, True)
+        
+        
+    # After
+    def tearDown(self):
+        print("Finishing test...")
 
 #if name == '__main__':
 #    unittest.main()
