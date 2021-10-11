04822707658e9906c64dec90ad8764e7f63b73a2
emilyrvillalba <56134620+emilyrvillalba@users.noreply.github.com>
2021-10-11 15:59:18 -0700

    removed some code that didn't work

--- Backend/test.py
+++ Backend/test.py
@@ -2,10 +2,6 @@ import unittest
 import main
 
 class TestStringMethods(unittest.TestCase):
-    # Before
-    def setUp(self):
-        print("\n")
-        print("Before testing...")
 
  
     #Flora
@@ -15,23 +11,6 @@ class TestStringMethods(unittest.TestCase):
     def test_check_username2(self):
         self.assertEqual(main.check_username("^*!(&#%($"), False)    
         
-    # Emily
-     def test_check_username_bool(self):
-        # ensures that this username is not taken
-        username = main.check_username("JohnnyJune")
-        self.assertFalse(username)
-     def test_check_valid_password(self):
-        # ensures that password satisfies length requirement
-        password = main.check_valid_password("supercalifragilisticexpialidocious")
-        self.assertEqual(password, True)
-        
-        
-    # After
-    def tearDown(self):
-        print("Finishing test...")
-
-#if name == '__main__':
-#    unittest.main()
 
 
 
