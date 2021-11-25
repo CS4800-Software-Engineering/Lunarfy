6530acc0c38758273d292ac974f3acca174d1ddd
emilyrvillalba <emilyrvillalba@gmail.com>
2021-11-24 21:08:10 -0800

    Game points show

--- Game/gameScript.js
+++ Game/gameScript.js
@@ -5,8 +5,8 @@ var points = 0
 // var correct = true
 const wordbank = ["take", "life", "back", "never","die", "away", "give", "time", "night", "day", "man", "woman", "dream", "world", "little","baby", "good", "again", "eyes", "mind", "heart", "world", "you", "breathe", "love", "fire", "dance", "moon", "sun", "star", "flower", "kiss", "lips", "sugar", "honey", "sweet", "girl", "boy", "everything", "lovely", "beautiful", "car", "music", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday", "december", "september", "august", "red", "blue", "yellow", "green", "orange", "purple", "black", "white", "sad", "happy", "mad", "slow", "fast", "talk", "call", "run", "sorry", "money", "myself", "young", "old", "school", "work"];
 var word = ''
+let time = 0.5 * 60;
 
-let time = 0.1 * 60; //minutes * 60 seconds
 
 function updateTimer() {
     const minutes = Math.floor(time / 60); // rounds a number DOWN to the nearest integer
@@ -19,38 +19,39 @@ function updateTimer() {
 
     if (time < 0) { //stop the setInterval whe time = 0 for avoid negative time
         clearInterval(refreshIntervalId);
-        // window.location.href = "https://pages.emilyrvillalba.repl.co/timeUp.html";
-        // document.getElementById("keywordAnswers").innerHTML = word
-        document.getElementById("timer").style.display = "none"
-        document.getElementById("prompt").style.display = "none"
-        document.getElementById("field").style.display = "none"
-        document.getElementById("check").style.display = "none"
-        document.getElementById("bf1").style.display = "none"
-        document.getElementById("bf2").style.display = "none"
-        document.getElementById("bf3").style.display = "none"
+        document.getElementById("timer").style.display = "none";
+        document.getElementById("prompt").style.display = "none";
+        document.getElementById("field").style.display = "none";
+        document.getElementById("check").style.display = "none";
+        document.getElementById("bf1").style.display = "none";
+        document.getElementById("bf2").style.display = "none";
+        document.getElementById("bf3").style.display = "none";
 
-        document.getElementById("timeup").style.display = "inline-block"
-        document.getElementById("songs").style.display = "inline-block"
-        document.getElementById("space").style.display = "inline-block"
+        document.getElementById("timeup").style.display = "inline-block";
+        document.getElementById("songs").style.display = "inline-block";
+        document.getElementById("space").style.display = "inline-block";
 
-        document.getElementById("keyword").style.display = "inline-block"        
-        document.getElementById("continue").style.display = "inline-block"
-        document.getElementById("generate").style.display = "none"
-        document.getElementById("end").style.display = "inline-block"
-        document.getElementById("b1").style.display = "inline-block"
-        document.getElementById("b2").style.display = "inline-block"
-        document.getElementById("b3").style.display = "inline-block"
-        document.getElementById("b4").style.display = "inline-block"
-        document.getElementById("b5").style.display = "inline-block"
+        document.getElementById("keyword").style.display = "inline-block";   
+        document.getElementById("continue").style.display = "inline-block";
+        document.getElementById("generate").style.display = "none";
+        document.getElementById("end").style.display = "inline-block";
+        document.getElementById("b1").style.display = "inline-block";
+        document.getElementById("b2").style.display = "inline-block";
+        document.getElementById("b3").style.display = "inline-block";
+        document.getElementById("b4").style.display = "inline-block";
+        document.getElementById("b5").style.display = "inline-block";
 
     }
 }
 
 
-function generateWord(){
-  time = 0.1 * 60;
+function generateWord(points=0){
+  // clearInterval(refreshIntervalId);
+  time = 0.5 * 60;
   refreshIntervalId = setInterval(updateTimer, 1000);
-  document.getElementById("points").style.display = "inline-block"
+  var pointStr = "Points: "+ points
+
+  document.getElementById("generate").style.display = "inline-block"
   document.getElementById("timer").style.display = "inline-block"
   document.getElementById("prompt").style.display = "inline-block"
   document.getElementById("field").style.display = "inline-block"
@@ -63,7 +64,7 @@ function generateWord(){
   document.getElementById("songs").style.display = "none"
   document.getElementById("space").style.display = "none"
   document.getElementById("continue").style.display = "none"
-  document.getElementById("end").style.display = "none"
+  document.getElementById("end").style.display = "inline-block"
 
   document.getElementById("intro").style.display = "none"
   document.getElementById("intro2").style.display = "none"
@@ -76,40 +77,91 @@ function generateWord(){
   word = wordbank[Math.floor(Math.random() * wordbank.length)];
   // alert(text)
   document.getElementById("keyword").innerHTML = word
+  document.getElementById("points").innerHTML = pointStr
+
 
 }
 
+function game3(){
+  var correct = "True"
+  var resultStr = "CORRECT";
 
-function game(){
-  var correct = true
-  // document.getElementById("timer").style.display = "none"
+  if (correct == "True"){
+    points = points + 1;
+    resultStr = "CORRECT";
+  }else{
+    resultStr = "INCORRECT";
+  }
+
+  alert(resultStr);
+  generateWord(points);
+}
+
+
+
+
+function game2(){
+  clearInterval(refreshIntervalId);
+  // document.getElementById("continue").style.display = "inline-block";
+  // document.getElementById("check").style.display = "none";
+  // document.getElementById("field").style.display = "none";
   // document.getElementById("prompt").style.display = "none"
-  // document.getElementById("field").style.display = "none"
-  document.getElementById("check").style.display = "none"
-  // document.getElementById("keyword").style.display = "none"
-  var resultStr = "Correct"
-  // document.getElementById("continue").style.display = "inline-block"
-  // document.getElementById("generate").style.display = "none"
-  // document.getElementById("end").style.display = "inline-block"
 
-  alert("HIIII")
-  // window.location.href = "https://pages.emilyrvillalba.repl.co/result.html";
-  if (correct){
+
+  // alert("Correct");
+  var correct = "True";
+
+  if (correct == "True"){
     points = points + 1;
 
-    pointStr = "Points: " + points 
-    document.getElementById("points").innerHTML = pointStr
-    document.getElementById("prompt").innerHTML = resultStr
-    // document.getElementById("correctOrNot").style.display = "inline-block"
-    document.getElementById("continue").style.display = "inline-block"
-    document.getElementById("end").style.display = "inline-block"
+    var pointStr = "Points: " + points;
+    var resultStr = "Correct";
+    document.getElementById("points").innerHTML = pointStr;
+    alert("result");
+    generateWord();
 
   }else{
-    alert("Incorrect")
-    generateWord()
+    alert("Incorrect");
+    generateWord();
   }
-  alert("GREAT JOB!!!!!")
+  generateWord();
+  alert("generate");
+
 }
+
+
+
+// function game(){
+//   var correct = true
+//   // document.getElementById("timer").style.display = "none"
+//   // document.getElementById("prompt").style.display = "none"
+//   // document.getElementById("field").style.display = "none"
+//   document.getElementById("check").style.display = "none"
+//   // document.getElementById("keyword").style.display = "none"
+//   var resultStr = "Correct"
+//   // document.getElementById("continue").style.display = "inline-block"
+//   // document.getElementById("generate").style.display = "none"
+//   // document.getElementById("end").style.display = "inline-block"
+
+//   alert("HIIII")
+//   // window.location.href = "https://pages.emilyrvillalba.repl.co/result.html";
+//   if (correct){
+//     points = points + 1;
+
+//     pointStr = "Points: " + points 
+//     document.getElementById("points").innerHTML = pointStr
+//     // document.getElementById("prompt").innerHTML = resultStr
+//     alert("result")
+//     // document.getElementById("correctOrNot").style.display = "inline-block"
+//     document.getElementById("continue").style.display = "inline-block"
+//     document.getElementById("end").style.display = "inline-block"
+
+//   }else{
+//     alert("Incorrect")
+//     generateWord()
+//   }
+//   alert("GREAT JOB!!!!!")
+// }
 
 // function updateTimer() {
 //   const minutes = Math.floor(time/60);
